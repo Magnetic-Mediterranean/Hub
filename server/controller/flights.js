3b4d5a9f130dfdccc33f0640e233695b180c5499
Michael Moriarty <mmoriarty316@gmail.com>
2021-04-06 22:43:04 -0700

    Create flights.js

---
+++ server/controller/flights.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const path = 'ec2-3-142-95-22.us-east-2.compute.amazonaws.com:3000';
+
+
+const flights = (req, res, next) => {
+  axios(path + req.url)
+  .then(({ data }) => {
+    res.send(data);
+    next();
+  })
+  .catch((err) => {
+    res.status(400);
+    next();
+  })
+}
+
+module.exports = { flights }
