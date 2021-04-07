531b6beddc23a75b689cd27e94c16c0bbb0725a9
Michael Moriarty <mmoriarty316@gmail.com>
2021-04-06 22:44:34 -0700

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { flights } = require('./controller/flights');
 
-
+router.get('/flights/*', flights);
 
 module.exports = router;
