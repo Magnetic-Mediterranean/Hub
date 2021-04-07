8a6e9d08a6bb774fe3a51b198ffefdf49a515b8c
Nilai Patel <nilaip96@gmail.com>
2021-04-07 13:24:16 -0700

    Fixes to activities hub

--- server/controller/activities.js
+++ server/controller/activities.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 
+const path = 'http://ec2-13-52-242-226.us-west-1.compute.amazonaws.com:3000'
+
 const activities = (req, res) => {
-  console.log(req.body);
-  axios.post('http://ec2-13-52-242-226.us-west-1.compute.amazonaws.com/activities', req.body)
-  .then((data) => {
+  axios.post(path + '/activities', req.body)
+  .then(({ data }) => {
     res.send(data);
   })
   .catch((err) => {
-    res.status(400);
+    res.send('error');
   })
 }
 
