5352c5214386e2dc2174af48db3fe6e2ff90f1fb
Nilai Patel <nilaip96@gmail.com>
2021-04-07 12:54:47 -0700

    Attempted to connection to Activities microservice

--- server/controller/activities.js
+++ server/controller/activities.js
@@ -1,11 +1,9 @@
 const axios = require('axios');
-const path = 'ec2-13-52-242-226.us-west-1.compute.amazonaws.com/activities';
-
 
 const activities = (req, res) => {
   console.log(req.body);
-  axios.post(path, req.body)
-  .then(({ data }) => {
+  axios.post('http://ec2-13-52-242-226.us-west-1.compute.amazonaws.com/activities', req.body)
+  .then((data) => {
     res.send(data);
   })
   .catch((err) => {
