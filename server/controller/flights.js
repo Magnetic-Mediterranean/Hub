c63f92a47b1ceb64a1f2feebc8c6e1e5b07b1e8e
Michael Moriarty <mmoriarty316@gmail.com>
2021-04-07 20:02:06 -0700

    Update flights.js

--- server/controller/flights.js
+++ server/controller/flights.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const path = 'ec2-3-142-95-22.us-east-2.compute.amazonaws.com:3000';
+const path = 'http://ec2-3-142-95-22.us-east-2.compute.amazonaws.com:3000';
 
 
 const flights = (req, res) => {
