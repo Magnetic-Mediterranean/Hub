58b311837048b6535bf1ee8a6a748860deff6a42
Kanchan Chauhan <kanchan827@gmail.com>
2021-04-07 20:05:07 -0700

    ready to deploy

--- server/controller/hotels.js
+++ server/controller/hotels.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 
 const hotels = (req, res) => {
+  console.log(req.url)
   axios.get(`http://52.91.136.121:3000${req.url}`).then((response) => {
+
     res.send(response.data);
   })
   .catch((err) => {
